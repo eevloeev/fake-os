d306a21a9efd0f8d03f3c7783fa30a1d38cfe515
Aslan Evloev <eevloeev@gmail.com>
2024-06-12 00:43:22 +0530

    perf: remove unusual renders of Datetime component

--- components/NavigationBar/components/Datetime/Datetime.tsx
+++ components/NavigationBar/components/Datetime/Datetime.tsx
@@ -1,32 +1,52 @@
 "use client"
 
 import Tooltip, { POSITION } from "@/shared/Tooltip/Tooltip"
-import { useEffect, useState } from "react"
+import { memo, useEffect, useState } from "react"
 import styles from "./Datetime.module.css"
 
-export default function Datetime() {
-  const [dateTime, setDateTime] = useState<Date>()
+function getTime(date: Date) {
+  return date.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  })
+}
+
+function getDate(date: Date) {
+  return date.toLocaleDateString()
+}
+
+function Datetime() {
+  const [time, setTime] = useState<string>()
+  const [date, setDate] = useState<string>()
 
   useEffect(() => {
-    setDateTime(new Date())
+    const newDate = new Date()
+    setTime(getTime(newDate))
+    setDate(getDate(newDate))
 
     const interval = setInterval(() => {
-      setDateTime(new Date())
+      const newDate = new Date()
+      const newTime = getTime(newDate)
+      const newDateTime = getDate(newDate)
+
+      if (newTime !== time) {
+        setTime(newTime)
+      }
+      if (newDateTime !== date) {
+        setDate(newDateTime)
+      }
     }, 1000)
 
     return () => {
       clearInterval(interval)
     }
-  }, [])
+  }, [date, time])
 
   return (
-    <Tooltip text={dateTime?.toLocaleDateString()} position={POSITION.TOP_LEFT}>
-      <div className={styles.time}>
-        {dateTime?.toLocaleTimeString([], {
-          hour: "2-digit",
-          minute: "2-digit",
-        }) ?? "00:00"}
-      </div>
+    <Tooltip text={date} position={POSITION.TOP_LEFT}>
+      <div className={styles.time}>{time ?? "00:00"}</div>
     </Tooltip>
   )
 }
+
+export default memo(Datetime)
